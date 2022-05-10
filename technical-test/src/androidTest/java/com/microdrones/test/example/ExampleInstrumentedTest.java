fd6811b1545f7c5f669c2cb8dfd992b29b91bf7c
shivam.m1 <shivam.m1@taskmo.com>
2022-05-11 01:53:18 +0530

    some changes

--- technical-test/src/androidTest/java/com/microdrones/test/example/ExampleInstrumentedTest.java
+++ technical-test/src/androidTest/java/com/microdrones/test/example/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.sdm.mirrar.library.test", appContext.getPackageName());
+        assertEquals("com.microdrones.technical_test.technical-test.test", appContext.getPackageName());
     }
 }
